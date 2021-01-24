c73951a2daa3f5c1b880828a9c13b97bebfef1bb
Nguyen-Van-Manh <manh117bg@gmail.com>
2021-01-25 00:38:36 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -49,7 +49,7 @@ async function bootstrap() {
         maxAge: 60000,
       },
       store: new MongoStore({
-        url: uri || "mongodb+srv://manh123:manhuetvnuk63j@cluster0.ntafe.mongodb.net/ebuy?retryWrites=true&w=majority"
+        url: process.env.DB_URL || "mongodb+srv://manh123:manhuetvnuk63j@cluster0.ntafe.mongodb.net/ebuy?retryWrites=true&w=majority"
       }),
     })
   )
